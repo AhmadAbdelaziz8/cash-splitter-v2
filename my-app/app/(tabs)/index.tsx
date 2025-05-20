1aa054b63f645535eddf5b2647da868015f31b29
iCobra3 <ahmadabdelaziz8@gmail.com>
2025-05-20 15:29:45 +0300

    improve the ui

--- my-app/app/(tabs)/index.tsx
+++ my-app/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { Image } from "expo-image";
+// import { Image } from "expo-image";
 import { router } from "expo-router";
 import { View, Text, TouchableOpacity } from "react-native";
 import "./../global.css";
@@ -11,36 +11,20 @@ export default function HomeScreen() {
   };
 
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-      headerImage={
-        <Image
-          source={require("@/assets/images/partial-react-logo.png")}
-          className="h-[200px] w-[290px] absolute bottom-0 left-0"
-        />
-      }
-    >
-      <View className="p-4 space-y-6">
-        <View className="mt-4 items-center">
-          <ThemedText type="title">Cash Splitter</ThemedText>
-        </View>
-
-        <View className="items-center">
-          <ThemedText>
-            Take a photo of your receipt and let AI split the costs among your
-            friends.
-          </ThemedText>
-        </View>
-
-        <TouchableOpacity
-          className="py-4 px-8 rounded-full items-center justify-center mt-5 bg-sky-600"
-          onPress={handleScanPress}
-        >
-          <Text className="mt-3 text-red text-lg font-bold">Scan Receipt</Text>
-        </TouchableOpacity>
-
-        {/* We'll add recent links section here later */}
+    <ParallaxScrollView>
+      <View className="items-center">
+        <ThemedText className="text-center">
+          Take a photo of your receipt and let AI split the costs among your
+          friends.
+        </ThemedText>
       </View>
+
+      <TouchableOpacity
+        className="py-4 px-8 rounded-full items-center justify-center mt-8 bg-sky-600 active:bg-sky-700 shadow-md"
+        onPress={handleScanPress}
+      >
+        <Text className="text-white text-lg font-bold">Scan Receipt</Text>
+      </TouchableOpacity>
     </ParallaxScrollView>
   );
 }
