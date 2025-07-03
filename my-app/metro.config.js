e374b66474250b47a5d9a6d3eef96b6145f4fd44
iCobra3 <ahmadabdelaziz8@gmail.com>
2025-07-03 09:48:39 +0300

    Fix metro config for NativeWind v4 compatibility

--- my-app/metro.config.js
+++ my-app/metro.config.js
@@ -1,6 +1,8 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: "./global.css" });
+// NativeWind v4 configuration
+config.transformer.unstable_allowRequireContext = true;
+
+module.exports = config;
